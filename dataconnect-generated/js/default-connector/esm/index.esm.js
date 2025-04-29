d3fd5461b1b9cd03981ebeeb56d5d9dc62f58aa0
tuahkm <tuahkm@gmail.com>
2025-04-29 23:22:40 +0000

    git commit -m "feat: complete e-commerce functionality with shopping cart, search, and product details"

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'mhp',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'mhp',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
