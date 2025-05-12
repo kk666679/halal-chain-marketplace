2e165e7365a2cf5ec33d8789916dd7fd9780132d
Kurnia Kadir <tuahkm@gmail.com>
2025-05-12 00:36:39 +0000

    update Header.jsx to use public/project to play video

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -128,7 +128,7 @@ export default function Header({ toggleSidebar }) {
           playsInline
           className="w-full h-full object-cover"
         >
-          <source src="/videos/chemmara-halal-chain.mp4" type="video/mp4" />
+          <source public="/projects/chemmara-halal-chain.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-black bg-opacity-30"></div> {/* Optional overlay */}
